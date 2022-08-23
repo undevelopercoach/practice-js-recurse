94f84a0f296ec5ade3bed33e310e9d469520ffe2
Will Britton <will@undeveloper.com>
2022-08-23 13:54:32 +0100

    Corrected binary search test

--- exercises/2-binarySearch.test.js
+++ exercises/2-binarySearch.test.js
@@ -39,8 +39,10 @@ it('returns -1 if it can\'t find the value', () => {
 it('finds the value in a long array with repeated values', () => {
   const arr = [1, 1, 1, 3, 3, 5, 6, 7, 9, 9, 13, 13, 13, 13, 13, 13, 13, 13, 14, 16, 17, 19, 20]
   const soughtValue = 13
+  // There's no guarantee that the search will find the first duplicated value, so check the range
   const result = findPositionOfValue(arr, soughtValue)
-  expect(result).toEqual(10)
+  expect(result >= 10).toBe(true)
+  expect(result <= 17).toBe(true)
 })
 
 it('returns -1 if the array is empty', () => {
